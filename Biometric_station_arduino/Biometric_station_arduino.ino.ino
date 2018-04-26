0dda6c8e287c51ce27c1c0537d55d6b93c509a72
Frederik Feys <frederik.feys99@gmail.com>
2018-04-26 22:43:17 +0200

    Temperature sensor works just wrong number

---
+++ Biometric_station_arduino/Biometric_station_arduino.ino.ino
@@ -0,0 +1,30 @@
+#include <LiquidCrystal.h>
+int potPin = 5;             //input read pin for LM35 is Analog Pin 5
+int temperature = 0;          //variable which will be calculated in process
+        
+long val=0;            //variable to store the value coming from the sensor
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+void setup()
+{
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+  Serial.println("LM35 Thermometer    ");
+  lcd.print("Thermometer    ");
+    }
+
+void loop ()            //loop below process
+{
+ 
+  val = analogRead(potPin);        //read the value of sensor
+  temperature = (5*val*100/1024);      //convert voltage to temperature
+  Serial.println ((float)temperature);
+     //print temperature value on serial screen
+  lcd.setCursor(0,1);
+lcd.print((float)temperature);
+ lcd.setCursor(3,1);
+lcd.print("D.Centigrade");
+Serial.println("D.Centigrade");
+  delay(2000);           //wait for 2seconds
+
+}
