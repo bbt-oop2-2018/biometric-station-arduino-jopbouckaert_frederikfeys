ce74784e6f6410374d9bd76e23800d870ecd8652
Frederik Feys <frederik.feys99@gmail.com>
2018-04-29 13:38:34 +0200

    Temperature now works perfectly

--- Biometric_station_arduino/Biometric_station_arduino.ino/Biometric_station_arduino/Biometric_station_arduino.ino
+++ Biometric_station_arduino/Biometric_station_arduino.ino/Biometric_station_arduino/Biometric_station_arduino.ino
@@ -1,71 +1,72 @@
-#include <Wire.h>
-#include <LiquidCrystal.h>
+#include <Wire.h> // Used to establied serial communication on the I2C bus
+#include "SparkFunTMP102.h" // Used to send and recieve specific information from our sensor
 
-//Declare RS, E, D4, D5, D6, D7
-LiquidCrystal lcd(7,8,9,10,11,12);
+// Connections
+// VCC = 3.3V
+// GND = GND
+// SDA = A4
+// SCL = A5
+const int ALERT_PIN = A3;
 
-void setup() 
-{
-  Serial.begin(115200);
-  
-  //Initialize I2C Communication
-  Wire.begin();
-  
-  //Initialize the LCD Display
-  lcd.begin(16, 2);
-  lcd.setCursor(0,0);
-  lcd.write("TEMP(C): ");
+TMP102 sensor0(0x48); // Initialize sensor at I2C address 0x48
+// Sensor address can be changed with an external jumper to:
+// ADD0 - Address
+//  VCC - 0x49
+//  SDA - 0x4A
+//  SCL - 0x4B
 
+void setup() {
+  Serial.begin(115200); // Start serial communication at 9600 baud
+  pinMode(ALERT_PIN,INPUT);  // Declare alertPin as an input
+  sensor0.begin();  // Join I2C bus
+
+  // Initialize sensor0 settings
+  // These settings are saved in the sensor, even if it loses power
+
+  // set the number of consecutive faults before triggering alarm.
+  // 0-3: 0:1 fault, 1:2 faults, 2:4 faults, 3:6 faults.
+  sensor0.setFault(0);  // Trigger alarm immediately
+
+  // set the polarity of the Alarm. (0:Active LOW, 1:Active HIGH).
+  sensor0.setAlertPolarity(1); // Active HIGH
+
+  // set the sensor in Comparator Mode (0) or Interrupt Mode (1).
+  sensor0.setAlertMode(0); // Comparator Mode.
+
+  // set the Conversion Rate (how quickly the sensor gets a new reading)
+  //0-3: 0:0.25Hz, 1:1Hz, 2:4Hz, 3:8Hz
+  sensor0.setConversionRate(2);
+
+  //set Extended Mode.
+  //0:12-bit Temperature(-55C to +128C) 1:13-bit Temperature(-55C to +150C)
+  sensor0.setExtendedMode(1);
+
+  //set T_HIGH, the upper limit to trigger the alert on
+  sensor0.setHighTempC(29.4); // set T_HIGH in C
+
+  //set T_LOW, the lower limit to shut turn off the alert
+  sensor0.setLowTempC(26.67); // set T_LOW in C
 }
 
-void loop() 
+void loop()
 {
-  //call the sensorRead function to retrieve the temperature
-  double temperature = sensorRead();
+  float temperature;
 
-  //Print the current temperature to the right of the label
-  lcd.setCursor(9,0);
-  lcd.print(temperature);
+  // Turn sensor on to start temperature measurement.
+  // Current consumtion typically ~10uA.
+  sensor0.wakeup();
 
-  //wait 1/2 second
-  delay(200);
-}
+  // read temperature data
+  temperature = sensor0.readTempC();
 
-double sensorRead(void)
-{
-  //temp holds the two bytes of data read back from the TMP102
-  uint8_t temp[2];
+  // Place sensor in sleep mode to save power.
+  // Current consumtion typically <0.5uA.
+  sensor0.sleep();
 
-  //tempc holds the modified combination of bytes
-  int16_t tempc;
-
-  //Point to device 0x48
-  Wire.beginTransmission(0x48);
-  //Point to register 0x00 (Temperature Register)
-  Wire.write(0x00);
-  //Relinquish master control of I2C line
-  Wire.endTransmission();
-  //delay to allow for sufficient conversion time
-  delay(10);
+  // Print temperature and alarm state
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
   
-  //Request temperature data
-  Wire.requestFrom(0x48, 2);
-
-    //if the two bytes are sucessfully returned
-    if (2 <= Wire.available())
-    {       
-      //read out the data
-      temp[0]  = Wire.read();
-      temp[1] = Wire.read();
-
-      //ignore the lower 4 bits of byte 2
-      temp[1] = temp[1] >> 4;
-      //combine to make one 12 bit binary number
-      tempc = ((temp[0] << 4) | temp[1]);
-
-      //Convert to celcius (0.0625C resolution) and return
-      return tempc*0.0625;
-    }
+  delay(1000);  // Wait 1000ms
 }
-
 
