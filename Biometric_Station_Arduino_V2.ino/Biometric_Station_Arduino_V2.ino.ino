1db3167d14fbd9071b12cee7c02100badfa57c35
Frederik Feys <frederik.feys99@gmail.com>
2018-05-18 09:08:45 +0200

    Final version

--- Biometric_Station_Arduino_V2.ino/Biometric_Station_Arduino_V2.ino.ino
+++ Biometric_Station_Arduino_V2.ino/Biometric_Station_Arduino_V2.ino.ino
@@ -1,15 +1,16 @@
+  #include <SparkFunTMP102.h>
+  #include <SparkFun_MMA8452Q.h>
   #include <LiquidCrystal.h>
   #include <Wire.h>
 
 
-  #define MMA8452_ADDRESS 0x1D
 
+  #define MMA8452_ADDRESS 0x1D
   #define OUT_X_MSB 0x01
   #define XYZ_DATA_CFG  0x0E
   #define WHO_AM_I   0x0D
   #define CTRL_REG1  0x2A
-
-  #define GSCALE 2 // Sets full-scale range to +/-2, 4, or 8g. Used to calc real g values.
+  #define GSCALE 2            // Sets full-scale range to +/-2, 4, or 8g. Used to calc real g values.
 
   LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
@@ -22,19 +23,13 @@
   float checksum;
   
   const byte SCALE = 2;
-  
   const byte dataRate = 0;
 
   int int1Pin = 2;  
-  int int2Pin = 3;
-
-  
+  int int2Pin = 3; 
 
   const int ALERT_PIN = A3;
-
   
-
-
   int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
   int blinkPin = 13;                // pin to blink led at each beat
   int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
@@ -47,34 +42,34 @@
   volatile boolean QS = false; 
   #define SERIAL_PLOTTER  2
   static int outputType = SERIAL_PLOTTER;
-
-
+  
   byte c;
-  #include "SparkFunTMP102.h"
-
+  
   TMP102 sensor0(0x48);
 
- 
+  byte heart[8] = {
+  B00000,
+  B00000,
+  B01010,
+  B11111,
+  B11111,
+  B01110,
+  B00100,
+};
 
-
-void setup() {
-  Serial.begin(115200);
   
+void setup() {
+  
+  Serial.begin(115200);
   Wire.begin();
 
   initMMA8452(); 
 
-  
-
- 
-
-  
-
+  lcd.createChar(0, heart);
+  lcd.begin(16, 2);      
   
 
   
-  
-
 
   pinMode(ALERT_PIN, INPUT); // Declare alertPin as an input
   sensor0.begin();  // Join I2C bus
@@ -93,19 +88,16 @@ void setup() {
 
   sensor0.setLowTempC(26.67); 
 
+  
   pinMode(blinkPin, OUTPUT);  
   pinMode(fadePin, OUTPUT);                      
   interruptSetup();   
 
 
-lcd.begin(16, 2);
-  // Print a message to the LCD.
-  
-
+  lcd.begin(16, 2);
 }
 
 
-//  Where the Magic Happens
 void loop() {
 
   float temperature;
@@ -118,7 +110,7 @@ void loop() {
 
   setTemperature(temperature);
 
-  delay(200);
+  delay(50);
 
   serialOutput() ;
 
@@ -134,7 +126,7 @@ void loop() {
   ledFadeToBeat();                      // Makes the LED Fade Effect Happen
   delay(20);                             //  take a break
 
-setHeartbeat(BPM);
+  setHeartbeat(BPM);
 
 
 //XYZ
@@ -162,30 +154,32 @@ setHeartbeat(BPM);
 createChecksum();
 concatString();
 
-lcd.setCursor(0,0);
-
-lcd.print(temperatureSent);
-lcd.print((char)223);
-lcd.print(String("C"));
-
-lcd.setCursor(8,0);
 
 
-lcd.print(BPMSent);
 
-lcd.setCursor(0,1);
-lcd.print(String("X: "));
-lcd.print(x);
+  lcd.setCursor(0,0);
 
-lcd.setCursor(9,1);
-lcd.print(String("Y: "));
-lcd.print(y);
+  lcd.print(temperatureSent);
+  lcd.print((char)223);
+  lcd.print(String("C"));
 
-lcd.setCursor(11,0);
-lcd.print(String("Z:"));
-lcd.print(z);
+  lcd.setCursor(7,0);
 
+  lcd.write(byte(0));
+  lcd.print(BPMSent);
+  lcd.print(String(" "));
 
+  lcd.setCursor(0,1);
+  lcd.print(String("X: "));
+  lcd.print(x);
+
+  lcd.setCursor(9,1);
+  lcd.print(String("Y: "));
+  lcd.print(y);
+
+  lcd.setCursor(11,0);
+  lcd.print(String("Z:"));
+  lcd.print(z);
 
 
 
@@ -318,32 +312,6 @@ void writeRegister(byte addressToWrite, byte dataToWrite)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 void setTemperature(float temperature) {
   temperatureSent = temperature;
 
@@ -374,6 +342,8 @@ void createChecksum(){
 }
 
 void concatString(){
-  Serial.println(String("ABCDE") + temperatureSent + String("|") + BPMSent + String("XXX") + x + String("YYY") + y + String("ZZZ") + z + String("CCC") + checksum +String("EDCBA"));
+  Serial.println(String("ABCDE") + temperatureSent + String("|") + BPMSent 
+  + String("XXX") + x + String("YYY") + y + String("ZZZ") + z + String("CCC") 
+  + checksum +String("EDCBA"));
 }
 
